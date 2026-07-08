c1399a3d4f54ca51c4bf42d4a7c09c10e8a435c9
darkademic <41052878+darkademic@users.noreply.github.com>
2026-07-08 20:05:28 +0100

    Ion Cannon & Lightning Storm always strikes center with first shot.

---
+++ OpenRA.Mods.CA/TraitsInterfaces.cs
@@ -0,0 +1,115 @@
+#region Copyright & License Information
+/**
+ * Copyright (c) The OpenRA Combined Arms Developers (see CREDITS).
+ * This file is part of OpenRA Combined Arms, which is free software.
+ * It is made available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version. For more information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using OpenRA.GameRules;
+using OpenRA.Graphics;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.CA.Traits
+{
+	// Allows modifying previews e.g. with WithColoredOverlayCA
+	public interface IActorPreviewRenderModifierInfo : ITraitInfoInterface
+	{
+		IActorPreviewRenderModifier GetPreviewRenderModifier(WorldRenderer wr, ActorInfo actorInfo, TypeDictionary inits, Color previewColor);
+	}
+
+	public interface IActorPreviewRenderModifier
+	{
+		IEnumerable<IRenderable> ModifyPreviewRender(WorldRenderer wr, IEnumerable<IRenderable> renderables, Rectangle bounds);
+		void Tick();
+	}
+
+	[RequireExplicitImplementation]
+	public interface ISmokeParticleInfo
+	{
+		string Image { get; }
+		string[] Sequences { get; }
+		string Palette { get; }
+
+		int[] Duration { get; }
+
+		WDist[] Speed { get; }
+
+		WDist[] Gravity { get; }
+
+		WeaponInfo Weapon { get; }
+
+		int TurnRate { get; }
+
+		int RandomRate { get; }
+	}
+
+	[RequireExplicitImplementation]
+	public interface IMultiWeaponImpactInfo
+	{
+		WeaponInfo Weapon { get; }
+		CVec[] ImpactOffsets { get; }
+		WDist RandomOffset { get; }
+		bool RandomImpactSequence { get; }
+		bool ExcludeFirstImpactFromRandomSequence { get; }
+		int[] Interval { get; }
+	}
+
+	public interface INotifyActivate { void Launching(Actor self); }
+
+	[RequireExplicitImplementation]
+	public interface IPointDefense
+	{
+		bool Destroy(WPos position, Player attacker, string type, ProjectileArgs args);
+	}
+
+	public interface INotifyPointDefenseHit
+	{
+		void Hit(int damagePrevented);
+	}
+
+	public interface IBotCAInfo : ITraitInfoInterface { string Name { get; } }
+	public interface IBotCA
+	{
+		bool IsEnemyUnit(Actor a);
+	}
+
+	[RequireExplicitImplementation]
+	public interface INotifyChronosphere { void Teleporting(WPos from, WPos to); }
+
+	public interface ILoadsOverlayPlayerPalettes { void LoadOverlayPlayerPalettes(WorldRenderer wr, string playerName, Color playerColor, bool replaceExisting); }
+
+	public interface INotifyPrismCharging { void Charging(Actor self, in Target target); }
+
+	[RequireExplicitImplementation]
+	public interface INotifyEnterTeleporter { void Charging(Actor self, Actor teleporter); }
+	public interface INotifyExitTeleporter { void Arrived(Actor self); }
+
+	[RequireExplicitImplementation]
+	public interface IBotAircraftBuilder { bool CanBuildMoreOfAircraft(ActorInfo actorInfo); }
+
+	[RequireExplicitImplementation]
+	public interface IPortableChronoModifier { int GetCooldownModifier(); int GetRangeModifier(); int GetExtraCharges(); }
+
+	[RequireExplicitImplementation]
+	public interface INotifyFallDown { void OnLanded(Actor self); }
+
+	[RequireExplicitImplementation]
+	public interface INotifyMindControlled { void MindControlled(Actor self, Actor master); void Released(Actor self, Actor master); }
+
+	[RequireExplicitImplementation]
+	public interface INotifyMindControlling { void MindControlling(Actor self, Actor slave); }
+
+	[RequireExplicitImplementation]
+	public interface INotifyCountChanged { void Incremented(string type); void Decremented(string type); }
+
+	[RequireExplicitImplementation]
+	public interface ISeedsResourceIntervalModifier { int GetModifier(); }
+
+	[RequireExplicitImplementation]
+	public interface INotifyAttachedTo { void Attached(Actor self, Actor attachedActor, Attachable attachable); void Detached(Actor self, Actor detachedActor, Attachable attachable); }
+}
